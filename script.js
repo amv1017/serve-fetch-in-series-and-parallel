0b2842a1d3921c51837d4c67b2cb8be6e7bd5d67
Alexander <amv1017@mail.ru>
2025-02-28 09:38:21 +0300

    separate function

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ const list = document.getElementById('list')
 const btnSequence = document.getElementById('btns')
 const btnParallel = document.getElementById('btnp')
 
+const fetchItem = async (val) => {
+  const res = await fetch(`/api/${val}`)
+  const { text } = await res.json()
+  // console.log(text)
+  const item = document.createElement('li')
+  item.innerText = text
+  list.append(item)
+}
+
 async function runInSequence() {
   list.innerHTML = ''
   loader.style.display = 'block'
   for (let val of values) {
-    const res = await fetch(`/api/${val}`)
-    const { text } = await res.json()
-    const item = document.createElement('li')
-    item.innerText = text
-    list.append(item)
-    console.log(text)
+    await fetchItem(val)
   }
   loader.style.display = 'none'
 }
@@ -24,12 +28,7 @@ async function runInParallel() {
   loader.style.display = 'block'
   await Promise.all(
     values.map(async (val) => {
-      const res = await fetch(`/api/${val}`)
-      const { text } = await res.json()
-      const item = document.createElement('li')
-      item.innerText = text
-      list.append(item)
-      console.log(text)
+      await fetchItem(val)
       loader.style.display = 'none'
     })
   )
